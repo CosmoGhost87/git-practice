423450438359823d8a8e126794d0b78d92f8552b
CosmoGhost87 <120912513+CosmoGhost87@users.noreply.github.com>
2026-09-16 16:30:11 +0300

    modify(dev3): изменен cfile

--- cfile.cpp
+++ cfile.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string greeting = "Hello, World!";
+    std::cout << greeting << std::endl;
     return 0;
 }
